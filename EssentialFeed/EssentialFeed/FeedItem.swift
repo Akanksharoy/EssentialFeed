4c86f5decba23fa968ac6753c0e181a5e1f1022e
Akanksha <akabksha.roy95@gmail.com>
2023-11-07 20:39:14 +0530

    Feed Item created

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,14 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Akanksha on 07/11/23.
+//
+
+import Foundation
+struct FeedItem{
+    let id:UUID
+    let description:String?
+    let location:String?
+    let imageURL:URL
+}
