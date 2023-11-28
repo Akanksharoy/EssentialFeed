6bd17d0b9b4aa540449b2239e33cf531b3362b58
Akanksha <akabksha.roy95@gmail.com>
2023-11-28 16:38:42 +0530

    Code refactoring

---
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedItem.swift
@@ -0,0 +1,14 @@
+//
+//  RemoteFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Animesh on 28/11/23.
+//
+
+import Foundation
+internal struct RemoteFeedItem: Decodable {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let image: URL
+}
---
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedImage.swift
@@ -0,0 +1,23 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Animesh on 28/11/23.
+//
+
+import Foundation
+
+public struct LocalFeedImage: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let url: URL
+    
+    public init(id: UUID, description: String?, location: String?, url: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.url = url
+    }
+}
+
