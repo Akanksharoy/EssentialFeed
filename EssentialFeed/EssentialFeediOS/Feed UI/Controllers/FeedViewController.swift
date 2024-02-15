bb784038488483dd79c8ecec88c68d08e6a1738c
Akanksha <akanksha.roy95@gmail.com>
2024-02-16 02:56:39 +0530

    Completed another lecture

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import EssentialFeed
 
 public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
     private var refreshController: FeedRefreshViewController?
@@ -18,41 +17,41 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
         self.init()
         self.refreshController = refreshController
     }
-
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         tableView.prefetchDataSource = self
         refreshControl = refreshController?.view
         refreshController?.refresh()
     }
-
+    
     public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tableModel.count
     }
-
+    
     public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return cellController(forRowAt: indexPath).view()
     }
-
+    
     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         cancelCellControllerLoad(forRowAt: indexPath)
     }
-
+    
     public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
         indexPaths.forEach { indexPath in
             cellController(forRowAt: indexPath).preload()
         }
     }
-
+    
     public func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
         indexPaths.forEach(cancelCellControllerLoad)
     }
-
+    
     private func cellController(forRowAt indexPath: IndexPath) -> FeedImageCellController {
         return tableModel[indexPath.row]
     }
-
+    
     private func cancelCellControllerLoad(forRowAt indexPath: IndexPath) {
         cellController(forRowAt: indexPath).cancelLoad()
     }
--- EssentialFeed/EssentialFeediOS/Feed UI/Models/FeedViewModel.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Models/FeedViewModel.swift
@@ -10,16 +10,16 @@ import EssentialFeed
 
 final class FeedViewModel {
     typealias Observer<T> = (T) -> Void
-    
+
     private let feedLoader: FeedLoader
-    
+
     init(feedLoader: FeedLoader) {
         self.feedLoader = feedLoader
     }
-    
+
     var onLoadingStateChange: Observer<Bool>?
     var onFeedLoad: Observer<[FeedImage]>?
-    
+
     func loadFeed() {
         onLoadingStateChange?(true)
         feedLoader.load { [weak self] result in
